1bbb1f73a8065a0c4f37609b1e29a997ef5a593b
Guillaume LADORME <gladorme@gmail.com>
2020-07-20 16:49:00 +0200

    #10 Add small validation

--- front/src/app/game/game.component.ts
+++ front/src/app/game/game.component.ts
@@ -13,22 +13,27 @@ export class GameComponent implements OnInit {
   yearGuess: number;
   score: number;
   currentRound: number;
+  buttonDisabled: boolean;
 
-  constructor(private gameService: GameService, private router: Router){
+  constructor(private gameService: GameService, private router: Router) {
+    this.buttonDisabled = true;
     this.gameService.score$.subscribe(score => this.score = score);
     this.gameService.currentRound$.subscribe(round => this.currentRound = round);
     this.gameService.currentImage$.subscribe(path => this.image = path);
-    this.gameService.getNextImage();
+    this.gameService.getNextImage().then(data => this.buttonDisabled = false);
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
+    if (this.yearGuess == null) { return; }
+    this.buttonDisabled = true;
     this.gameService.getScore(this.yearGuess).then(guessResult => this.gameService.score$.next(this.score + guessResult.score));
+    this.yearGuess = null;
     if (this.currentRound < 10) {
       this.gameService.currentRound$.next(this.currentRound + 1);
-      this.gameService.getNextImage();
+      this.gameService.getNextImage().then(data => this.buttonDisabled = false);
     } else {
       this.router.navigate(['./result/']);
     }
