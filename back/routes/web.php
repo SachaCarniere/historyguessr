85ece36d859208b7a3d5dd2b6f67f71759ebb871
Guillaume LADORME <gladorme@gmail.com>
2020-07-28 15:46:34 +0200

    Remove useless back routes :+1:

--- back/routes/web.php
+++ back/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::middleware('cors')->group(function () {
     Route::post('game', 'GameController@create');
