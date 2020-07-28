b35d53fbe4fda88e41d27f350598bb5b08448d61
Guillaume LADORME <gladorme@gmail.com>
2020-07-28 15:47:08 +0200

    Add Gatling simulations (Soak + Stress) :star:

---
+++ other/simulations/historyguessr/HomePageStress.scala
@@ -0,0 +1,24 @@
+package historyguessr
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class HomePageStress extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://89.234.182.58")
+		.acceptHeader("*/*")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+
+	val scn = scenario("Stress Test - HomePage")
+		.exec(http("GET /").get("http://89.234.182.58/"))
+				.pause(5)
+
+
+	setUp(scn.inject(atOnceUsers(1000))).protocols(httpProtocol)
+}
---
+++ other/simulations/historyguessr/StartGameSoak.scala
@@ -0,0 +1,25 @@
+package historyguessr
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class StartGameSoak extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://89.234.182.58:8000")
+		.acceptHeader("*/*")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0")
+
+	val scn = scenario("Soak Test - POST Game")
+		.during(1 minutes, "Soak Test - POST Game") {
+			exec(http("POST /game").post("http://89.234.182.58:8000/game"))
+				.pause(5)
+		}
+
+	setUp(scn.inject(atOnceUsers(200))).protocols(httpProtocol)
+}
